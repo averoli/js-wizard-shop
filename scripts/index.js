6a651d752629136586a62a6c332bce6bd6af6541
Averoli <afanasenkavolha@gmail.com>
2022-04-08 17:07:56 +0200

    branch developr

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,26 @@
 const form = document.getElementById("form");
-const field = form.querySelectorAll(".input-profile");
+const fields = form.querySelectorAll(".input-profile");
 
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPass = document.getElementById("confirmPass");
 
-const clearBtn = document.getElementById("clear-btn");
 const nextBtn = document.getElementById("next-btn");
+document
+  .getElementById("clear-btn")
+  .addEventListener("click", function (event) {
+    for (let i = 0; i < fields.length; i++) {
+      fields[i].value = "";
+    }
+  });
 
 function userNameValidation() {
+  if (username.value.indexOf(" ") >= 0) {
+    console.log("FALSE!");
+    username.style.borderColor = "red!important";
+    console.log(username.style.borderColor);
+  }
   if (username.value.length < 5) {
     console.log("username less then 5");
   } else if (username.value.length > 20) {
@@ -28,12 +39,12 @@ function emailValidation() {
 }
 
 function passwordValidation() {
-  console.log(password.value);
   const passCheck =
     /(?=.*[0-9](?=.*[!@#$%^&*]))(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,20}/;
   if (password.value.match(passCheck)) {
     console.log("Correct");
   } else {
+    password.style.borderColor = "red";
     console.log("The text must contain at least ...");
   }
 }
@@ -46,16 +57,18 @@ function confirmPassValidation() {
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+
+  //   for (let i = 0; i < fields.length; i++) {
+  //     if (fields[i].value == false) {
+  //       console.log("Field connot be empty!");
+  //       fields[i].style.borderColor = "red";
+  //     }
+  //     else {
+  //       fields[i].style.borderColor = "green";
+  //     }
+  //   }
   userNameValidation();
   emailValidation();
   passwordValidation();
   confirmPassValidation();
-  for (let i = 0; i < field.length; i++) {
-    if (field[i].value == false) {
-      console.log("Field connot be empty!");
-      field[i].style.borderColor = "red";
-    } else {
-      field[i].style.borderColor = "green";
-    }
-  }
 });
