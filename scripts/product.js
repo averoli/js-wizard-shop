ff292f877afcebbf0486227b2b6ddd14f74aa702
Ivan Escribano <ivanescribano1998@gmail.com>
2022-04-11 18:04:02 +0200

    Display profile and hide product

--- scripts/product.js
+++ scripts/product.js
@@ -1,6 +1,15 @@
-const greenBtn = document.querySelector(".green");
-greenBtn.addEventListener("mouseover", changeImg);
-const productImage = document.querySelector(".product-images");
-function changeImg(params) {
-  productImage.style.backgroundImage = `url(../assets/green-sweatshirt.png)`;
+const shopNowBtn = document.querySelector(".product-details-shopNow button");
+// const sections = document.querySelectorAll("body> section");
+const profileContainer = document.getElementById("profile");
+const productContainer = document.getElementById("mainProduct");
+shopNowBtn.addEventListener("click", showProfile);
+function showProfile() {
+  profileContainer.style.display = "block";
+  productContainer.style.display = "none";
 }
+// const greenBtn = document.querySelector(".green");
+// greenBtn.addEventListener("mouseover", changeImg);
+// const productImage = document.querySelector(".product-images");
+// function changeImg(params) {
+//   productImage.style.backgroundImage = `url(../assets/green-sweatshirt.png)`;
+// }
