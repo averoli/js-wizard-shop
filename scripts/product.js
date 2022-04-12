55a02ed2dbca60acebf9d187ffcc44ee16d59542
MatiasPatrignani <100035710+MatiasPatrignani@users.noreply.github.com>
2022-04-12 13:39:35 +0200

    add reset shipping

--- scripts/product.js
+++ scripts/product.js
@@ -1,11 +1,12 @@
 const shopNowBtn = document.querySelector(".product-details-shopNow button");
 const sections = document.querySelectorAll("body> section");
+const profile = document.getElementById("#profile")
 
 shopNowBtn.addEventListener("click", showProfile);
-function showProfile(id) {
+function showProfile() {
   for (const step of sections) {
     console.log(step.id);
-    if (step.id === id) {
+    if (step.id === "profile") {
       step.style.display = "block";
     } else {
       step.style.display = "none";
